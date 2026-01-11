3640e83fe97f128dcf71de98a4aa3882aa928b32
DonaldKisaka <kisakadonald1@gmail.com>
2026-01-11 11:41:41 +0300

    Added getUsers, unlock user, disableuser and enable user services

--- Identity-Access-Management/src/main/java/identityaccessmanagement/example/Identity/Access/Management/service/UserService.java
+++ Identity-Access-Management/src/main/java/identityaccessmanagement/example/Identity/Access/Management/service/UserService.java
@@ -4,7 +4,11 @@ import identityaccessmanagement.example.Identity.Access.Management.dto.UserRespo
 import identityaccessmanagement.example.Identity.Access.Management.mapper.UserMapper;
 import identityaccessmanagement.example.Identity.Access.Management.model.User;
 import identityaccessmanagement.example.Identity.Access.Management.repository.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserService {
@@ -27,5 +31,38 @@ public class UserService {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("User not found"));
         return userMapper.toResponse(user);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    public Page<UserResponseDto> getAllUsers(Pageable pageable) {
+        return userRepository.findAll(pageable)
+                .map(userMapper::toResponse);
+    }
+
+    @Transactional
+    @PreAuthorize("hasRole('ADMIN')")
+    public void unlockUser(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        user.unlock();
+        userRepository.save(user);
+    }
+
+    @Transactional
+    @PreAuthorize("hasRole('ADMIN')")
+    public void disableUser(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        user.setEnabled(false);
+        userRepository.save(user);
+    }
+
+    @Transactional
+    @PreAuthorize("hasRole('ADMIN')")
+    public void enableUser(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        user.setEnabled(true);
+        userRepository.save(user);
     }
 }
