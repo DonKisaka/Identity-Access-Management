b554fef9285ced776121ae1c40e217c5c52c2ab0
DonaldKisaka <kisakadonald1@gmail.com>
2026-01-11 11:34:04 +0300

    Added mapper annotation

--- Identity-Access-Management/src/main/java/identityaccessmanagement/example/Identity/Access/Management/mapper/PermissionMapper.java
+++ Identity-Access-Management/src/main/java/identityaccessmanagement/example/Identity/Access/Management/mapper/PermissionMapper.java
@@ -2,7 +2,9 @@ package identityaccessmanagement.example.Identity.Access.Management.mapper;
 
 import identityaccessmanagement.example.Identity.Access.Management.dto.PermissionResponseDto;
 import identityaccessmanagement.example.Identity.Access.Management.model.Permission;
+import org.mapstruct.Mapper;
 
+@Mapper(componentModel = "spring")
 public interface PermissionMapper {
     PermissionResponseDto toResponse(Permission permission);
 }
