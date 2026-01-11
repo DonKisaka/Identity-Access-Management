1ae7c1bbf12577d228064976cc6b8087f9bd75a5
DonaldKisaka <kisakadonald1@gmail.com>
2026-01-11 12:46:13 +0300

    PermissionController

---
+++ Identity-Access-Management/src/main/java/identityaccessmanagement/example/Identity/Access/Management/controller/PermissionController.java
@@ -0,0 +1,33 @@
+package identityaccessmanagement.example.Identity.Access.Management.controller;
+
+import identityaccessmanagement.example.Identity.Access.Management.dto.PermissionRequestDto;
+import identityaccessmanagement.example.Identity.Access.Management.dto.PermissionResponseDto;
+import identityaccessmanagement.example.Identity.Access.Management.service.PermissionService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/permissions")
+public class PermissionController {
+
+    private final PermissionService permissionService;
+
+    public PermissionController(PermissionService permissionService) {
+        this.permissionService = permissionService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<PermissionResponseDto>> getAllPermissions() {
+        return ResponseEntity.ok(permissionService.getAllPermissions());
+    }
+
+    @PostMapping()
+    public ResponseEntity<PermissionResponseDto> createPermission(@Valid  @RequestBody PermissionRequestDto dto) {
+        PermissionResponseDto permissionResponseDto = permissionService.createPermission(dto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(permissionResponseDto);
+    }
+}
