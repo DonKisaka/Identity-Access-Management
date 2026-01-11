0164147e2682b3beaadd753f3619c313b72e7833
DonaldKisaka <kisakadonald1@gmail.com>
2026-01-11 12:52:40 +0300

    Added admin endpoints

--- Identity-Access-Management/src/main/java/identityaccessmanagement/example/Identity/Access/Management/controller/UserController.java
+++ Identity-Access-Management/src/main/java/identityaccessmanagement/example/Identity/Access/Management/controller/UserController.java
@@ -2,12 +2,11 @@ package identityaccessmanagement.example.Identity.Access.Management.controller;
 
 import identityaccessmanagement.example.Identity.Access.Management.dto.UserResponseDto;
 import identityaccessmanagement.example.Identity.Access.Management.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/users")
@@ -31,5 +30,28 @@ public class UserController {
     public ResponseEntity<UserResponseDto> getUserByUsername(@PathVariable String username) {
         UserResponseDto user = userService.getUserByUsername(username);
         return ResponseEntity.ok(user);
+    }
+
+    @GetMapping()
+    public ResponseEntity<Page<UserResponseDto>> getAllUsers(Pageable pageable) {
+        return ResponseEntity.ok(userService.getAllUsers(pageable));
+    }
+
+    @PostMapping("/{userId}/unlock")
+    public ResponseEntity<Void> unlockUser(@PathVariable Long userId) {
+        userService.unlockUser(userId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{userId}/disable")
+    public ResponseEntity<Void> disableUser(@PathVariable Long userId) {
+        userService.disableUser(userId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/{userId}/enable")
+    public ResponseEntity<Void> enableUser(@PathVariable Long userId) {
+        userService.enableUser(userId);
+        return ResponseEntity.ok().build();
     }
 }
