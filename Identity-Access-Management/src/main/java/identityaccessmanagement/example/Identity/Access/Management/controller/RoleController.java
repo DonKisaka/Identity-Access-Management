f4d1deada63e42ecd1cb9cfd690e9d687fcc97f4
DonaldKisaka <kisakadonald1@gmail.com>
2026-01-13 14:20:16 +0300

    Role controller

--- Identity-Access-Management/src/main/java/identityaccessmanagement/example/Identity/Access/Management/controller/RoleController.java
+++ Identity-Access-Management/src/main/java/identityaccessmanagement/example/Identity/Access/Management/controller/RoleController.java
@@ -6,7 +6,6 @@ import identityaccessmanagement.example.Identity.Access.Management.service.RoleS
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
